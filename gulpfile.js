d1ee774833392b550153a359b4b0b52fc76075fc
Anna <anna.ch453@gmail.com>
2018-02-17 18:19:24 +0300

    автоматизирует

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,43 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
 
-gulp.task("style", function() {
+var del = require("del");
+
+gulp.task("images", function () {
+  return gulp.src("build/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("style", function () {
   gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +48,41 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]);
+});
+
+gulp.task("copy", function () {
+  return gulp.src(
+    [
+      "source/fonts/**/*{woff,woff2}",
+      "source/img/**",
+      "source/js/**"
+    ], {
+      base: "source"
+    }
+  )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "images",
+    "style",
+    "html",
+    done
+  );
 });
